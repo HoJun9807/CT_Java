cc7685cb2cf0c5f4f53a30d7948d2884024e7d11
임호준 <ghwns5087@naver.com>
2024-03-09 12:45:36 +0900

    Update 수열과 구간 쿼리 2.java

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,32 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
+
         int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
 
-        for(int i = 0; i < queries.length; i++) {
-            int[] query = queries[i];
-            int s = query[0];
-            int e = query[1];
-            int k = query[2];
+        for (int idx = 0; idx < queries.length; idx++) {
+            int[] query = queries[idx];
+            int s = query[0], e = query[1], k = query[2];
 
-            int min = 1000000;
-            int count = 0;
-            for(int j = s; j < e + 1; j++) {
-                if(arr[j] > k) {
-                    if(min > arr[j]) {
-                        min = arr[j];    
-                    }
-                } else {
-                    count++;
+            for (int i = s; i <= e; i++) {
+                if (k < arr[i]) {
+                    answer[idx] = answer[idx] == -1 ? arr[i] : Math.min(answer[idx], arr[i]);
                 }
             }
-
-            if(count == e - s + 1) {
-                answer[i] = -1;
-            } else {
-                answer[i] = min;    
-            }
         }
-
         return answer;
     }
 }

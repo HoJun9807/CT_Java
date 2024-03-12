1bd0162fb259d72092eb47923235a3ae5aa3d986
임호준 <ghwns5087@naver.com>
2024-03-12 18:08:43 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        
+        int year = A - 543;
+
+      
+        System.out.print(year);
+     
+    }
+
+
+}
